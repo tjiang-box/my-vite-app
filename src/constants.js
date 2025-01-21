a9b7bdf6ada6ad85c9f7a3b7366358098465745e
Jerry Jiang <tjiang@box.com>
2025-01-21 17:48:21 -0600

    install buie dependencies and create initial contentPreview and contentSidebar

---
+++ src/constants.js
@@ -0,0 +1,14 @@
+// Do not use these constants in your code, they are just for the demos
+
+const ROOTFOLDER_ID = "69083462919";
+const TOKEN = "P1n3ID8nYMxHRWvenDatQ9k6JKzWzYrz";
+const FILE_ID = "415542803939";
+const FEATURE_FLAGS = {
+  activityFeed: {
+    appActivity: {
+      enabled: false,
+    },
+  },
+};
+
+export { ROOTFOLDER_ID, TOKEN, FILE_ID, FEATURE_FLAGS };
